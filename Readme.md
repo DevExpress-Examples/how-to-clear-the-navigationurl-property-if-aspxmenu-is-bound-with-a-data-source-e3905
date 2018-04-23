926a641a5e37bae7156bf54148d7137fcb3c80da
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# How to clear the NavigationUrl property if ASPxMenu is bound with a data source
+
+
+<p>This example demonstrates how to clear the <a href="http://documentation.devexpress.com/#AspNet/DevExpressWebASPxMenuMenuItem_NavigateUrltopic"><u>MenuItem.NavigateUrl</u></a> property to prevent redirecting to another page on tablet device browsers. ASPxMenu is bound with a data source.</p><p><strong>See also:</strong><br />
+<a href="https://www.devexpress.com/Support/Center/p/K18563">How to customize ASPxMenu for Touch Screen browsers</a></p><p><a href="https://www.devexpress.com/Support/Center/p/E3904">ASPxMenu - How to clear the NavigationUrl property if items are created at design time</a><br />
+<a href="https://www.devexpress.com/Support/Center/p/E3906">ASPxMenu - How to support subitem opening and page redirecting on tablet device browsers</a></p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,31 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+
+Imports DevExpress.Web.ASPxMenu
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Private correctItems As Boolean = False ' for the demo purpose only
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+	End Sub
+
+	Protected Sub ASPxMenu1_ItemDataBound(ByVal source As Object, ByVal e As MenuItemEventArgs)
+		'Uncomment the following code to use it in a real application
+		'if ((e.Item.Parent != null) && (!String.IsNullOrEmpty(e.Item.Parent.NavigateUrl))) {
+		'    e.Item.Parent.NavigateUrl = null;
+		'}
+
+
+		'The following code is used for the demo purpose only. Do not use it in a real scenario
+		If correctItems Then
+			If (e.Item.Parent IsNot Nothing) AndAlso ((Not String.IsNullOrEmpty(e.Item.Parent.NavigateUrl))) Then
+				e.Item.Parent.NavigateUrl = Nothing
+			End If
+		ElseIf e.Item.Text = "Corrected Menu" Then
+			correctItems = True
+		End If
+	End Sub
+End Class
